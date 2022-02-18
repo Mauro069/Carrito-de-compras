8c9990410530e0973881e6cd4a568b315a07fabf
ÂMauro069 <maurovera069@gmail.com>
2022-02-18 17:21:07 -0300

    Explicacion del context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,14 +1,19 @@
 import { createContext, useState } from "react";
 
+/* Creamos el context, se le puede pasar un valor inicial */
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  /* Creamos un estado para el carrito, que sera un array vacio que luego tendra objetos */
   const [cartItems, setCartItems] = useState([]);
 
+  /* Creamos la funcion para agregar productos al carrito */
   const AddItemToCart = (item) => {
     setCartItems((prevState) => {
+      /* Nos fijamos si en el estado anterior se encuentra el producto con su id */
       const inCart = prevState.find((cartItem) => cartItem.id === item.id);
 
+      /* Si esta el producto en el carrito le sumamos 1 al amount (cantidad) sino no hacemos nada */
       if (inCart) {
         return prevState.map((cartItem) =>
           cartItem.id === item.id
@@ -20,16 +25,19 @@ export const CartProvider = ({ children }) => {
         );
       }
 
+      /* Si el producto no esta en el carrito devolvemos el estado anterior mas el nuevo producto */
       return [...prevState, { ...item, amount: 1 }];
     });
   };
 
+  /* Creamos la funcion para borrar productos del carrito */
   const DeleteItemToCart = (itemId) => {
+    /* Filtramos el carrito y devolvemos todos, menos el que tiene la id que le pasamos */
     setCartItems(cartItems.filter((cartItem) => cartItem.id !== itemId));
   };
 
   return (
-    /* El value tengo que pasarlo como objeto pq sino rompe */
+    /* Envolvemos el children con el provider y le pasamos un objeto con las propiedades que necesitamos por value */
     <CartContext.Provider
       value={{ cartItems, AddItemToCart, DeleteItemToCart }}
     >
