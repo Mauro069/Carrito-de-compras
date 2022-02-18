b484a08223548b57a456594201e6c789b474ff45
ÂMauro069 <maurovera069@gmail.com>
2022-02-18 17:09:02 -0300

    Array de productos de prueba

---
+++ src/data/Data.js
@@ -0,0 +1,44 @@
+export const ProductsData = [
+  {
+    id: 1,
+    price: 10,
+    amount: 0,
+    name: "Hamburguesa",
+    img: "https://image.freepik.com/vector-gratis/ilustracion-icono-dibujos-animados-hamburguesa-queso_138676-2450.jpg",
+  },
+  {
+    id: 2,
+    price: 15,
+    amount: 0,
+    name: "Pizza",
+    img: "https://image.freepik.com/vector-gratis/flying-slice-of-pizza-cartoon-vector-illustration-concepto-comida-rapida-vector-aislado-estilo-dibujos-animados-plana_138676-1934.jpg",
+  },
+  {
+    id: 3,
+    price: 8,
+    amount: 0,
+    name: "Fideos",
+    img: "https://image.freepik.com/vector-gratis/huevo-carne-fideos-ramen-dibujos-animados-palillos_138676-2543.jpg",
+  },
+  {
+    id: 4,
+    price: 12,
+    amount: 0,
+    name: "Tacos",
+    img: "https://image.freepik.com/vector-gratis/cute-taco-chef-thumbs-up-cartoon-icon-illustration-concepto-icono-profesion-alimentaria-aislado-estilo-dibujos-animados-plana_138676-3074.jpg",
+  },
+  {
+    id: 5,
+    price: 6,
+    amount: 0,
+    name: "Pancho",
+    img: "https://image.freepik.com/vector-gratis/ilustracion-icono-dibujos-animados-hotdog_138676-2449.jpg",
+  },
+  {
+    id: 6,
+    price: 3,
+    amount: 0,
+    name: "Papas Fritas",
+    img: "https://image.freepik.com/vector-gratis/ilustracion-icono-dibujos-animados-papas-fritas-concepto-icono-comida-rapida-aislado-estilo-dibujos-animados-plana_138676-2923.jpg",
+  },
+];
