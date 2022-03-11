94480d7f30f8ad8f07db9bd17f2a2bf5ea168f47
ÂMauro069 <maurovera069@gmail.com>
2022-03-11 19:36:22 -0300

    Conecte a la api

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,79 +1,62 @@
 import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 /* Creamos el context, se le puede pasar un valor inicial */
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   /* Creamos un estado para el carrito */
-  const [cartItems, setCartItems] = useState(() => {
-    try {
-      /* Verificamos si hay productos en el local storage,
-      si hay algo lo parseamos porque se guarda como string 
-      y si no hay nada devolvemos un array vacio */
-      const productosEnLocalStorage = localStorage.getItem("cartProducts");
-      return productosEnLocalStorage ? JSON.parse(productosEnLocalStorage) : [];
-    } catch (error) {
-      return [];
-    }
-  });
+  const [cartItems, setCartItems] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  /* Cada vez que se actualize el carrito seteamos el local storage para guardar los productos */
-  useEffect(() => {
-    localStorage.setItem("cartProducts", JSON.stringify(cartItems));
-  }, [cartItems]);
-
-  /* Creamos la funcion para agregar productos al carrito */
-  const AddItemToCart = (product) => {
-    /* Recibimos un producto y nos fijamos si ya esta en el carrito */
-    const inCart = cartItems.find(
-      (productInCart) => productInCart.id === product.id
-    );
-
-    /* Si el producto se encuentra en el carrito, recorremos el carrito
-    y al producto le sumamos uno a la cantidad, sino retornamos el carrito como estaba */
-    if (inCart) {
-      setCartItems(
-        cartItems.map((productInCart) => {
-          if (productInCart.id === product.id) {
-            return { ...inCart, amount: inCart.amount + 1 };
-          } else return productInCart;
-        })
-      );
-      /* Si el producto no se encuentra al carrito, lo agregamos y dejamos en uno la cantidad */
-    } else {
-      setCartItems([...cartItems, { ...product, amount: 1 }]);
-    }
+  const getProducts = async () => {
+    await axios
+      .get("http://localhost:4000/products")
+      .then(({ data }) => setProducts(data.products));
   };
 
-  /* Creamos la funcion para borrar productos del carrito */
-  const DeleteItemToCart = (productId) => {
-    /* Buscamos el producto con su id */
-    const inCart = cartItems.find(
-      (productInCart) => productInCart.id === productId
-    );
+  const getProductsCart = async () => {
+    return await axios
+      .get("http://localhost:4000/products-cart")
+      .then(({ data }) => setCartItems(data.productsCart))
+      .catch((error) => console.error(error));
+  };
 
-    /* Si la cantidad del producto es igual a 1, filtramos el carrito y lo sacamos */
-    if (inCart.amount === 1) {
-      setCartItems(
-        cartItems.filter((productInCart) => productInCart.id !== productId)
-      );
+  useEffect(() => {
+    getProducts();
+    getProductsCart();
+  }, []);
+
+  const addItemToCart = async (product) => {
+    const { name, img, price } = product;
+
+    await axios.post("http://localhost:4000/products-cart", { name, img, price });
+
+    getProducts();
+    getProductsCart();
+  };
+
+  const editItemToCart = async (id, query, amount) => {
+    if (query === "del" && amount === 1) {
+      await axios
+        .delete(`http://localhost:4000/products-cart/${id}`)
+        .then(({ data }) => console.log(data));
     } else {
-      /* Si la cantidad es mayor a 1, recorremos el carrito
-      y al producto le restamos uno en su cantidad, sino devolvemos el carrito como estaba */
-      setCartItems(
-        cartItems.map((productInCart) => {
-          if (productInCart.id === productId) {
-            return { ...inCart, amount: inCart.amount - 1 };
-          } else return productInCart;
+      await axios
+        .put(`http://localhost:4000/products-cart/${id}?query=${query}`, {
+          amount,
         })
-      );
+        .then(({ data }) => console.log(data));
     }
+
+    getProducts();
+    getProductsCart();
   };
 
   return (
     /* Envolvemos el children con el provider y le pasamos un objeto con las propiedades que necesitamos por value */
     <CartContext.Provider
-      value={{ cartItems, AddItemToCart, DeleteItemToCart }}
+      value={{ cartItems, products, addItemToCart, editItemToCart }}
     >
       {children}
     </CartContext.Provider>
